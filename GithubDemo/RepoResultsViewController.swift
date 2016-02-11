08798f532cc302c785527cf2d6f336de3dae2183
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-10 19:04:58 -0600

    Update records

--- GithubDemo/RepoResultsViewController.swift
+++ GithubDemo/RepoResultsViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  GithubDemo
 //
-//  Created by Nhan Nguyen on 5/12/15.
-//  Copyright (c) 2015 codepath. All rights reserved.
+//  Created by Aditya Purandare
+//  Copyright (c) 2015. All rights reserved.
 //
 
 import UIKit
