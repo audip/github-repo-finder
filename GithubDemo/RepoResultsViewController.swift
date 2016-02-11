6ba2512851a245a11e7dd40cf1cabcbaae0bdae5
Aditya Purandare <aditya.p1993@hotmail.com>
2016-02-10 21:06:58 -0600

    Search functionality works

--- GithubDemo/RepoResultsViewController.swift
+++ GithubDemo/RepoResultsViewController.swift
@@ -18,6 +18,7 @@ class RepoResultsViewController: UIViewController, UITableViewDataSource, UITabl
     @IBOutlet weak var tableView: UITableView!
     
     var repos: [GithubRepo]!
+    var filteredList: [GithubRepo]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +55,7 @@ class RepoResultsViewController: UIViewController, UITableViewDataSource, UITabl
             }
             
             self.repos = newRepos
+            self.filteredList = self.repos
             self.tableView.reloadData()
 
             MBProgressHUD.hideHUDForView(self.view, animated: true)
@@ -62,7 +64,7 @@ class RepoResultsViewController: UIViewController, UITableViewDataSource, UITabl
         })
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let repos = repos {
+        if let repos = filteredList {
             return repos.count
         } else {
             return 0
@@ -71,7 +73,7 @@ class RepoResultsViewController: UIViewController, UITableViewDataSource, UITabl
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("RepoCell", forIndexPath: indexPath) as! RepoCell
         
-        cell.repo = repos[indexPath.row]
+        cell.repo = filteredList![indexPath.row]
         
         return cell
     }
@@ -80,6 +82,27 @@ class RepoResultsViewController: UIViewController, UITableViewDataSource, UITabl
 // SearchBar methods
 extension RepoResultsViewController: UISearchBarDelegate {
 
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            filteredList = repos
+        } else {
+            // The user has entered text into the search box
+            // Use the filter method to iterate over all items in the data array
+            // For each item, return true if the item should be included and false if the
+            // item should NOT be included
+            filteredList = repos.filter({(repo: GithubRepo) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                if repo.name!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        }
+        tableView.reloadData()
+    }
     func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
         searchBar.setShowsCancelButton(true, animated: true)
         return true;
@@ -93,6 +116,8 @@ extension RepoResultsViewController: UISearchBarDelegate {
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
         searchBar.text = ""
         searchBar.resignFirstResponder()
+        filteredList = repos
+        tableView.reloadData()
     }
 
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
